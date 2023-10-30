96eb8b2df020225d29b0282093d42a1f6b76bdb9
Your Name <you@example.com>
2023-10-30 14:52:56 +0200

    ADD NAV

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
 import SpecsIndex from "./component/specs/Index.jsx";
+import NavBar from "./component/navBar/Index.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+const About = () => (
+  <div
+    style={{
+      color: "#eee",
+      fontSize: "6rem",
+      margin: "2rem",
+    }}
+  >
+    About Page
+  </div>
+);
+const Blog = () => (
+  <div
+    style={{
+      color: "#eee",
+      fontSize: "6rem",
+      margin: "2rem",
+    }}
+  >
+    Blog Page
+  </div>
+);
 
 function App() {
-  return <SpecsIndex />;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<SpecsIndex />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Blog" element={<Blog />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/navBar/Index.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom";
+import "./nav.css";
+export default function Index() {
+  const nav = [
+    {
+      href: "/",
+      name: "Space",
+      icon: "-",
+    },
+    {
+      href: "/About",
+      name: "About",
+      icon: "-",
+    },
+    {
+      href: "/Blog",
+      name: "Blog",
+      icon: "-",
+    },
+  ];
+  return (
+    <div className="nav-bar">
+      <div className="logo">S P E C S</div>
+      {nav.map((n, i) => (
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending ? "nav pending" : isActive ? "nav active" : "nav"
+          }
+          key={i}
+          to={n.href}
+        >
+          <p>{n.name}</p>
+        </NavLink>
+      ))}
+    </div>
+  );
+}
