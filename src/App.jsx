7a3cc99554cff9702a019895acee017be72901a7
Your Name <you@example.com>
2023-11-02 15:10:17 +0200

    try  the fix 404 bug in gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Specs } from "./component/specs/Spacs.jsx";
 import { NavBar } from "./component/navBar/NavBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AddNewSpecs } from "./component/specs/addNewSpecs/AddNewSpecs.jsx";
 import "./App.css";
 
@@ -43,20 +43,21 @@ const Blog = () => (
 function App() {
   return (
     <div className="grid grid-cols-[161px_calc(100%_-161px)] min-w-full min-h-screen bg-[#21213E] font-sans">
-      <Router basename={import.meta.env.DEV ? "/" : "/specs/"}>
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/specs/"}>
         <NavBar />
 
         <div className="min-w-[calc(100%_-_161px)] h-auto">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" x element={<Home />} />
             <Route path="/specs" element={<Specs />} />
             <Route path="/specs/add-a-new-specs" element={<AddNewSpecs />} />
 
             <Route path="/About" element={<About />} />
             <Route path="/Blog" element={<Blog />} />
+            <Route path="*" element={<h1 className="m-8 text-5xl">Not Found</h1>} />
           </Routes>
         </div>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
