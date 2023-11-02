6a01fb16ee1b37b0db323468d2afa85d61a589c5
Your Name <you@example.com>
2023-11-02 23:41:50 +0200

    fix bugs

--- src/component/specs/addNewSpecs/AddNewSpecs.jsx
+++ src/component/specs/addNewSpecs/AddNewSpecs.jsx
@@ -1,46 +1,108 @@
 import { useState } from "react";
 import { RenderStep } from "./steps/RenderStep";
+import { useNavigate } from "react-router-dom";
+import { Step1 } from "./steps/step1";
+import { Step2 } from "./steps/step2";
+import { Step3 } from "./steps/step3";
+import { Step4 } from "./steps/step4";
+import { Step5 } from "./steps/step5";
+import { Step6 } from "./steps/step6";
 
 export function AddNewSpecs() {
+  const navigate = useNavigate();
   const [step, setStep] = useState(1);
 
-  const progress = [
+  const stepData = [
     {
       ind: 1,
       title: "Some Text",
+      btnR: {
+        titleR: "Close",
+        callBackR: () => navigate("/specs"),
+      },
+      btnL: {
+        titleL: "Next",
+        callBackL: () => setStep(2),
+      },
+      Component: () => <Step1 {...{ setStep }} />,
     },
     {
       ind: 2,
       title: "Some Text",
+      btnR: {
+        titleR: "Prev",
+        callBackR: () => setStep(1),
+      },
+      btnL: {
+        titleL: "Next",
+        callBackL: () => setStep(3),
+      },
+      Component: () => <Step2 {...{ setStep }} />,
     },
     {
       ind: 3,
       title: "KPls",
+      btnR: {
+        titleR: "Prev",
+        callBackR: () => setStep(2),
+      },
+      btnL: {
+        titleL: "Next",
+        callBackL: () => setStep(4),
+      },
+      Component: () => <Step3 {...{ setStep }} />,
     },
     {
       ind: 4,
       title: "Some Text",
+      btnR: {
+        titleR: "Prev",
+        callBackR: () => setStep(3),
+      },
+      btnL: {
+        titleL: "Next",
+        callBackL: () => setStep(5),
+      },
+      Component: () => <Step4 {...{ setStep }} />,
     },
     {
       ind: 5,
       title: "Some Text",
+      btnR: {
+        titleR: "Prev",
+        callBackR: () => setStep(4),
+      },
+      btnL: {
+        titleL: "Next",
+        callBackL: () => setStep(6),
+      },
+      Component: () => <Step5 {...{ setStep }} />,
     },
     {
       ind: 6,
       title: "Some Text",
+      btnR: {
+        titleR: "Prev",
+        callBackR: () => setStep(5),
+      },
+      btnL: {
+        titleL: "Submit",
+        callBackL: () => navigate("/specs"),
+      },
+      Component: () => <Step6 {...{ setStep }} />,
     },
   ];
 
   return (
     <div className="w-full mt-24 flex justify-start items-center flex-col">
-      <ul className="flex m-0 p-0 relative select-none	">
-        {progress.map((pro) => (
+      <ul className="flex max-w-[770px] m-0 p-0 relative select-none	w-full justify-evenly">
+        {stepData.map((pro) => (
           <li
             key={pro.ind}
             className={
               pro.ind === step
-                ? "list-none w-[110px] rounded-[16px] text-center relative flex justify-center"
-                : "list-none  w-[110px] text-center relative flex justify-center rounded-full"
+                ? "list-none min-w-[15%] rounded-[16px] text-center relative flex justify-center"
+                : "list-none min-w-[15%] text-center relative flex justify-center rounded-full"
             }
           >
             <div
@@ -53,7 +115,7 @@ export function AddNewSpecs() {
           </li>
         ))}
       </ul>
-      <div>{<RenderStep {...{ step, setStep }} />}</div>
+      <div>{<RenderStep {...{ step, stepData }} />}</div>
     </div>
   );
 }
--- src/component/specs/addNewSpecs/steps/RenderStep.jsx
+++ src/component/specs/addNewSpecs/steps/RenderStep.jsx
@@ -1,145 +1,43 @@
-import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
-import { Step1 } from "./step1";
-import { Step2 } from "./step2";
-import { Step3 } from "./step3";
-import { Step4 } from "./step4";
-import { Step5 } from "./step5";
-import { Step6 } from "./step6";
 
-const Steps = ({ step, setStep }) => {
-  switch (step) {
-    case 1:
-      return (
-        <div className="px-6 py-5 mt-12 min-w-[500px] min-h-[340px] bg-[#121231] rounded-xl flex flex-col justify-between">
-          <Step1 {...{ setStep }} />
-          <div className="flex justify-between">
-            <NavLink
-              to={"/specs"}
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-            >
-              Close
-            </NavLink>
-            <div
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-              onClick={() => setStep(2)}
-            >
-              Next
-            </div>
-          </div>
+let StepItem = ({ stepData, step }) => {
+  const {
+    Component,
+    btnR: { titleR, callBackR },
+    btnL: { titleL, callBackL },
+  } = stepData[step - 1];
+
+  return (
+    <div className="px-6 py-5 mt-12 min-w-[500px] min-h-[340px] bg-[#121231] rounded-xl flex flex-col justify-between">
+      <Component />
+      <div className="flex justify-between">
+        <div
+          onClick={callBackR}
+          to={"/specs"}
+          className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
+        >
+          {titleR}
         </div>
-      );
-    case 2:
-      return (
-        <div className="px-6 py-5 mt-12 min-w-[500px] min-h-[340px] bg-[#121231] rounded-xl flex flex-col justify-between">
-          <Step2 {...{ setStep }} />
-          <div className="flex justify-between">
-            <div
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-              onClick={() => setStep(1)}
-            >
-              Prev
-            </div>
-            <div
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-              onClick={() => setStep(3)}
-            >
-              Next
-            </div>
-          </div>
+        <div
+          className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
+          onClick={callBackL}
+        >
+          {titleL}
         </div>
-      );
-    case 3:
-      return (
-        <div className="px-6 py-5 mt-12 min-w-[500px] min-h-[340px] bg-[#121231] rounded-xl flex flex-col justify-between">
-          <Step3 {...{ setStep }} />
-          <div className="flex justify-between">
-            <div
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-              onClick={() => setStep(2)}
-            >
-              Prev
-            </div>
-            <div
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-              onClick={() => setStep(4)}
-            >
-              Next
-            </div>
-          </div>
-        </div>
-      );
-    case 4:
-      return (
-        <div className="px-6 py-5 mt-12 min-w-[500px] min-h-[340px] bg-[#121231] rounded-xl flex flex-col justify-between">
-          <Step4 {...{ setStep }} />
-          <div className="flex justify-between">
-            <div
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-              onClick={() => setStep(3)}
-            >
-              Prev
-            </div>
-            <div
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-              onClick={() => setStep(5)}
-            >
-              Next
-            </div>
-          </div>
-        </div>
-      );
-    case 5:
-      return (
-        <div className="px-6 py-5 mt-12 min-w-[500px] min-h-[340px] bg-[#121231] rounded-xl flex flex-col justify-between">
-          <Step5 {...{ setStep }} />
-          <div className="flex justify-between">
-            <div
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-              onClick={() => setStep(4)}
-            >
-              Prev
-            </div>
-            <div
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-              onClick={() => setStep(6)}
-            >
-              Next
-            </div>
-          </div>
-        </div>
-      );
-    case 6:
-      return (
-        <div className="px-6 py-5 mt-12 min-w-[500px] min-h-[340px] bg-[#121231] rounded-xl flex flex-col justify-between">
-          <Step6 {...{ setStep }} />
-          <div className="flex justify-between">
-            <div
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-              onClick={() => setStep(5)}
-            >
-              Prev
-            </div>
-            <NavLink
-              to={"/specs"}
-              className="pt-1 pb-1 bg-[#21213E] select-none text-white rounded-md text-center w-24 text-base font-bold cursor-pointer hover:opacity-70"
-            >
-              Submit
-            </NavLink>
-          </div>
-        </div>
-      );
-  }
+      </div>
+    </div>
+  );
 };
 
-export const RenderStep = ({ step, setStep }) => <Steps {...{ step, setStep }} />;
+export const RenderStep = ({ step, stepData }) => <StepItem {...{ step, stepData }} />;
 
 RenderStep.propTypes = {
   step: PropTypes.number,
   setStep: PropTypes.func,
+  stepData: PropTypes.array,
 };
 
-Steps.propTypes = {
+StepItem.propTypes = {
   step: PropTypes.number,
-  setStep: PropTypes.func,
+  stepData: PropTypes.array,
 };
